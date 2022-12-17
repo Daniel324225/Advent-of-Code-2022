5a0861d41223e93818db24b30eaf04b02848306f
Daniel324225 <dan324225@gmail.com>
2022-12-17 19:43:59 +0100

    day 17 part 2

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -42,7 +42,18 @@ fn collision(rock: &[[u8; 4]; 4], x: usize, y: usize, board: &Vec<[u8; 9]>) -> b
     false
 }
 
-fn part1(input: &str) -> Result<String, String> {
+fn find_repeating<T: Eq>(range: &[T]) -> Option<usize> {
+    let len = range.len();
+
+    for sub_len in 1.max(len / 3)..len / 2 {
+        if range[len - sub_len * 2..len - sub_len].eq(&range[len - sub_len..]) {
+            return Some(sub_len);
+        }
+    }
+    None
+}
+
+fn implementation(input: &str, amount: usize) -> Result<String, String> {
     let input: Vec<isize> = input
         .chars()
         .map(|c| match c {
@@ -52,20 +63,29 @@ fn part1(input: &str) -> Result<String, String> {
         })
         .collect::<Result<_, _>>()?;
 
-    let mut wind = input.iter().copied().cycle();
-
-    let mut moves = Vec::new();
+    let mut rocks = ROCKS.iter().enumerate().cycle();
+    let mut wind = input.iter().copied().enumerate().cycle();
 
     let mut board: Vec<[u8; 9]> = vec![[b'#'; 9]; 1];
     let mut max = 0;
 
-    for (kind, rock) in ROCKS.iter().enumerate().cycle().take(2022) {
+    let mut moves = Vec::new();
+    let mut heights = Vec::new();
+
+    let mut to_add = None;
+
+    let mut i = 0;
+    while i < amount {
+        i += 1;
+        let (kind, rock) = rocks.next().unwrap();
         board.resize(max + 8, *b"#       #");
         let start = max + 4;
         let mut y = start;
         let mut x = 3isize;
+        let (mut wind_idx, mut wind_dir);
         loop {
-            let new_x = x + wind.next().unwrap();
+            (wind_idx, wind_dir) = wind.next().unwrap();
+            let new_x = x + wind_dir;
             if !collision(rock, new_x as usize, y, &board) {
                 x = new_x;
             }
@@ -86,14 +106,28 @@ fn part1(input: &str) -> Result<String, String> {
             }
         }
 
-        moves.push((kind, x, start - y));
+        if to_add.is_none() {
+            moves.push((kind, wind_idx, x, start - y));
+            if let Some(len) = find_repeating(&moves) {
+                let rocks_left = amount - i;
+                let height_diff = max - heights[heights.len() - len];
+                let batches = rocks_left / len;
+                to_add = Some(height_diff * batches);
+                i += batches * len;
+            }
+            heights.push(max);
+        }
     }
 
-    Ok(max.to_string())
+    Ok((max + to_add.unwrap_or(0)).to_string())
 }
 
-fn part2(_: &str) -> Result<String, String> {
-    Ok("".to_string())
+fn part1(input: &str) -> Result<String, String> {
+    implementation(input, 2022)
+}
+
+fn part2(input: &str) -> Result<String, String> {
+    implementation(input, 1000000000000)
 }
 
 advent_of_code::aoc_main!(17);
